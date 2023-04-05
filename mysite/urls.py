53d188d64f541afb637ca11fdca01a1b8c6bfa3f
ankit <ankitkmr885@gmail.com>
2023-04-06 02:52:30 +0530

    2.2

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,3 +27,8 @@ urlpatterns = [
   
 ]
 # manually added for versal
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
