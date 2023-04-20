417a3ea5185909fb594448055c9ed53005afb6d9
ankit <ankitkmr885@gmail.com>
2023-04-21 03:04:33 +0530

    1.1

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
-echo " BUILD START"
+# echo " BUILD START"
+# python3.9 -m pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# echo " BUILD END" 
+
+echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
+mkdir -p staticfiles_build
+python3.9 manage.py collectstatic --noinput --clear --verbosity 0
+echo "BUILD END"
--- mysite/settings.py
+++ mysite/settings.py
@@ -133,5 +133,5 @@ STATICFILES_DIRS = [
 
 # added manually for vercel
 import os 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
