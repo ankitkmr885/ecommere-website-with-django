a1f5338daa995b1196b5019be0c10130e266c3a1
ankit <ankitkmr885@gmail.com>
2023-04-25 02:25:10 +0530

    without vercel config

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,9 +26,3 @@ urlpatterns = [
     path('login/',include('system.urls'))
   
 ]
-# manually added for versal
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
